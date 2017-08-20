fecc8c264bd7eb5686ad17d04c8a32913f017e22
yandrapfss <budiantoyandra@gmail.com>
2017-08-20 15:37:37 +0700

    redirect view

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url('', admin.site.urls),
     url(r'^filer/', include('filer.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
